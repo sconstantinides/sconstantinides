f7dd47b9e666eeef7a2f1db1efaa5bb95bb39a37
Stelios Constantinides <sconstantinides@gmail.com>
2015-07-27 15:22:33 -0500

    basic content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("Hi there");
+$(function() {
+  $('.container').scroll(function() {
+    if($(this).scrollTop() < 50) {
+      $(".container").css('background-color', '#A2DED0');
+    } else {
+      $(".container").css('background-color', '#68C3A3');
+    }
+  });
+});

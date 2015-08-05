a656a1129f399f1bce787bcd011fd9514ed95934
Stelios Constantinides <sconstantinides@gmail.com>
2015-08-05 18:27:34 -0500

    added cool button transitions

--- dist/all.js
+++ dist/all.js
@@ -1,3 +1,26 @@
-function email(){
+
+$(function() {
+  $('.primary-link').click(function(event){
+    event.preventDefault();
+    var href = $(this).attr('href');
+    var height = $(document).height();
+
+    window.setTimeout(function() {
+      redirect(href);
+    }, 600);
+
+    $(this).attr('href', '');
+    $(this).css('left', $(this).offset().left);
+    $(this).css('top', $(this).offset().top);
+    $(this).addClass('active');
+    $(this).animate({ width: '100%', height: height, left: 0, top: 0 });
+  });
+});
+
+function redirect(href) {
+  window.location = href;
+}
+
+function email() {
   alert('Get in touch: sconstantinides [at] gmail [dot] com');
 }

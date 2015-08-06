b5b884f75c1a8223cb3b5a91d1d120eb96e729fe
Stelios Constantinides <sconstantinides@gmail.com>
2015-08-06 16:02:22 -0500

    content to fade in

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function() {
 
     window.setTimeout(function() {
       redirect(href);
-    }, 600);
+    }, 500);
 
     $(this).attr('href', '');
     $(this).css('left', $(this).offset().left);
@@ -15,6 +15,8 @@ $(function() {
     $(this).addClass('active');
     $(this).animate({ width: '100%', height: height, left: 0, top: 0 });
   });
+
+  $('.fade-in').fadeIn();
 });
 
 function redirect(href) {

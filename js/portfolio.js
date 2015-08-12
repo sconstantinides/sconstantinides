7dc431c7bf1459932adcc31865f5c45ea9682280
Stelios Constantinides <sconstantinides@gmail.com>
2015-08-11 20:54:28 -0500

    clean up thumbnails and add notably

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,8 +2,14 @@ $(function() {
   animateBreadcrumbs();
 
   $('.thumb').click(function() {
+    var image_str = $(this).css('background-image');
+    var image = image_str.substr(4, image_str.length - 5);
+
     $(this).addClass('current').siblings().removeClass('current');
-    $(this).siblings('.large').css('opacity', 0).attr('src', $(this).attr('src')).animate({opacity: 1}, 300);
+    $(this).siblings('.large').css({
+      opacity: 0,
+      'max-width': $(this).css('max-width')
+    }).attr('src', image).animate({opacity: 1}, 300);
 
     var caption = $(this).siblings('.caption');
     caption.find('.number').text($(this).index() + 1);

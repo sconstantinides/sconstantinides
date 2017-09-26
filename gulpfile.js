e4ed189090fc3a607a617292d0b8eeb9fbd21cbc
Stelios Constantinides <sconstantinides@gmail.com>
2017-09-26 11:32:44 -0500

    cleanups and remove pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,75 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    clean = require('gulp-clean'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    order = require('gulp-order'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    cleanCSS = require('gulp-clean-css'),
-    pug = require('gulp-pug'),
-    webserver = require('gulp-webserver');
+  jshint = require('gulp-jshint'),
+  clean = require('gulp-clean'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify'),
+  order = require('gulp-order'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  concat = require('gulp-concat'),
+  cleanCSS = require('gulp-clean-css'),
+  webserver = require('gulp-webserver');
 
 gulp.task('lint', function() {
-    gulp.src('js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+  gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
-gulp.task('clean', function () {
-	gulp.src('dist/*', { read: false })
-		.pipe(clean());
+gulp.task('clean', function() {
+  gulp.src('dist/*', { read: false })
+    .pipe(clean());
 });
 
 gulp.task('scripts', function() {
-    gulp.src('js/*')
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist'));
+  gulp.src('js/*')
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('styles', function() {
-    gulp.src('scss/*.scss')
-        .pipe(order([
-            'reset.scss',
-            'base.scss',
-            '*'
-        ]))
-        .pipe(sass())
-        .on('error', onError)
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(concat('all.css'))
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename('all.min.css'))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('templates', function() {
-    gulp.src('templates/[^_]*.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('./'));
+  gulp.src('scss/*.scss')
+    .pipe(order([
+      'reset.scss',
+      'base.scss',
+      '*'
+    ]))
+    .pipe(sass())
+    .on('error', onError)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(concat('all.css'))
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename('all.min.css'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('webserver', function() {
-    gulp.src('./')
-        .pipe(webserver({
-            fallback: 'index.html',
-            livereload: true,
-            open: false,
-            port: 3000
-        }));
+  gulp.src('./')
+    .pipe(webserver({
+      fallback: 'index.html',
+      livereload: true,
+      open: false,
+      port: 3000
+    }));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('js/**/*', ['lint', 'scripts']);
-    gulp.watch('scss/**/*', ['styles']);
-    gulp.watch('templates/**/*', ['templates']);
+  gulp.watch('js/**/*', ['lint', 'scripts']);
+  gulp.watch('scss/**/*', ['styles']);
+  gulp.watch('templates/**/*', ['templates']);
 });
 
 function onError(err) {
-    console.log(err);
-    this.emit('end');
+  console.log(err);
+  this.emit('end');
 }
 
-gulp.task('default', ['lint', 'clean', 'scripts', 'styles', 'templates', 'webserver', 'watch']);
+gulp.task('default', ['lint', 'clean', 'scripts', 'styles', 'webserver', 'watch']);
